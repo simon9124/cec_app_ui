ad7dc7667d591bc95444c534c529b2c5b6ada7ce
simon9124 <435776313@qq.com>
2020-02-26 19:02:00 +0800

    一些微调

--- src/permission.js
+++ src/permission.js
@@ -1,20 +1,16 @@
-import router from './router';
-import store from './store';
-import {
-  Message
-} from 'element-ui';
-import NProgress from 'nprogress'; // progress bar
-import 'nprogress/nprogress.css'; // progress bar style
-import {
-  getToken
-} from '@/utils/auth'; // get token from cookie
-import getPageTitle from '@/utils/get-page-title';
+import router from "./router";
+import store from "./store";
+import { Message } from "element-ui";
+import NProgress from "nprogress"; // progress bar
+import "nprogress/nprogress.css"; // progress bar style
+import { getToken } from "@/utils/auth"; // get token from cookie
+import getPageTitle from "@/utils/get-page-title";
 
 NProgress.configure({
   showSpinner: false
 }); // NProgress Configuration
 
-const whiteList = ['/login']; // no redirect whitelist
+const whiteList = ["/login"]; // no redirect whitelist
 
 router.beforeEach(async (to, from, next) => {
   // start progress bar
@@ -27,10 +23,10 @@ router.beforeEach(async (to, from, next) => {
   const hasToken = getToken();
 
   if (hasToken) {
-    if (to.path === '/login') {
+    if (to.path === "/login") {
       // if is logged in, redirect to the home page
       next({
-        path: '/'
+        path: "/"
       });
       NProgress.done();
     } else {
@@ -40,13 +36,13 @@ router.beforeEach(async (to, from, next) => {
       } else {
         try {
           // get user info
-          await store.dispatch('user/getInfo');
+          await store.dispatch("user/getInfo");
 
           next();
         } catch (error) {
           // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken');
-          Message.error(error || 'Has Error');
+          await store.dispatch("user/resetToken");
+          Message.error(error || "Has Error");
           next(`/login?redirect=${to.path}`);
           NProgress.done();
         }
