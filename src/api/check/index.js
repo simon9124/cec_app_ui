869a44f169a907f9fe5dd507a92e848f7a74511e
simon9124 <435776313@qq.com>
2020-02-27 18:17:32 +0800

    检测列表：接口更新

--- src/api/check/index.js
+++ src/api/check/index.js
@@ -1,9 +1,7 @@
 // import axios from '@/libs/api.request.mock';
 import axios from "@/libs/api.request";
 
-import {
-  objectMerge
-} from "@/libs/util";
+import { objectMerge } from "@/libs/util";
 
 // 获取电机检测列表
 export const getCheckList = () => {
@@ -16,13 +14,13 @@ export const getCheckList = () => {
 // 获取电机检测列表（后端分页）
 export const getCheckListByPage = (pageIndex, pageSize) => {
   return axios.request({
-    url: `/api/qcResult/finByPage?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+    url: `/api/qcResult/finByPage?pageIndex=${pageIndex}&pageSize=${pageSize}&qc1result=1&qc2result=1`,
     method: "get"
   });
 };
 
 // 根据id获取检测详情
-export const getCheckProduct = (id) => {
+export const getCheckProduct = id => {
   return axios.request({
     url: `/api/product/get?id=${id}`,
     method: "get"
@@ -30,7 +28,7 @@ export const getCheckProduct = (id) => {
 };
 
 // 更新外观检测
-export const updateQc3 = (data) => {
+export const updateQc3 = data => {
   return axios.request({
     url: "/api/product/updateQc3",
     method: "put",
@@ -47,14 +45,16 @@ export const findUnqualified = (lineNo, qcIndex) => {
 };
 
 // 根据条件获取不合格产品列表 - 后端分页
-export const findUnqualifiedWithPage = (object) => {
-  const params = objectMerge({
+export const findUnqualifiedWithPage = object => {
+  const params = objectMerge(
+    {
       pageSize: object.pageSize,
       pageIndex: object.pageNum,
       lineNo: object.lineNo,
       isResolved: object.isResolved,
       qcIndex: object.tabSelected
-    }, {}
+    },
+    {}
     // object.filterFormData
   );
   return axios.request({
@@ -65,7 +65,7 @@ export const findUnqualifiedWithPage = (object) => {
 };
 
 // 根据条件获取错误原因下拉列表
-export const getDropDownList = (qcIndex) => {
+export const getDropDownList = qcIndex => {
   return axios.request({
     url: `/api/Unqualified/GetDropDownList?qcIndex=${qcIndex}`,
     method: "get"
@@ -73,7 +73,7 @@ export const getDropDownList = (qcIndex) => {
 };
 
 // 编辑错误原因
-export const updateCheckReason = (data) => {
+export const updateCheckReason = data => {
   return axios.request({
     url: "/api/qcResult/setUnqualifiedProductReason",
     method: "post",
@@ -85,7 +85,7 @@ export const updateCheckReason = (data) => {
 };
 
 // 根据条研码查询检测结果（外层collapse）
-export const findByBarcode = (barcode) => {
+export const findByBarcode = barcode => {
   return axios.request({
     url: `/api/qcResult/findByBarcode?barcode=${barcode}`,
     method: "get"
